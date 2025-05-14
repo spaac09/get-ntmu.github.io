d65ffc1139ee97390048438d226f425b8ab7136d
aubymori <aubyomori@gmail.com>
2025-05-14 07:30:45 -0500

    Initial commit

---
+++ static/ntmu.js
@@ -0,0 +1,75 @@
+let page = {
+    onHashChange()
+    {
+        let hash = location.hash.replace(/^#!\//, "");
+        let parts = hash.split("/");
+        this.navigate(parts);
+    },
+
+    async navigate(urlParts)
+    {
+        let pageContent = document.getElementById("page-content");
+        let spinner = document.getElementById("spinner");
+
+        pageContent.innerHTML = "";
+        spinner.hidden = false;
+
+        let template = "404";
+        let data = {};
+        switch (urlParts[0])
+        {
+            case "":
+                template = "home";
+                break;
+            case "download":
+            {
+                template = "download";
+                let r = await fetch("https://api.github.com/repos/aubymori/OpenWithEx/releases"); // debug
+                if (r.status != 200)
+                    break;
+
+                let json;
+                try
+                {
+                    json = await r.json();
+                } catch (e) { break; }
+
+                data.releases = [];
+                for (const release of json)
+                {
+                    let rdata = {};
+                    rdata.name = release.name;
+                    rdata.url = release.html_url;
+                    rdata.date = new Date(release.published_at).toLocaleDateString("ja-JP"); // YYYY/MM/DD forever
+                    for (const asset of release.assets)
+                    {
+                        if (asset.name == "OpenWithEx-setup-x64.exe")
+                        {
+                            rdata.download_x64 = asset.browser_download_url;
+                        }
+                    }
+                    data.releases.push(rdata);
+                }
+
+                break;
+            }
+            case "packs":
+                template = "packs";
+                break;
+        }
+
+        pageContent.innerHTML = nunjucks.render(template + ".html", data);
+        spinner.hidden = true;
+    },
+
+    init()
+    {
+        nunjucks.configure("templates", {
+            web: { useCache: true }
+        });
+        window.addEventListener("hashchange", this.onHashChange.bind(this));
+        this.onHashChange();
+    }
+};
+
+page.init();
